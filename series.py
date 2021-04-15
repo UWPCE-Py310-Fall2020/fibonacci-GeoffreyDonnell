599c26aece441ca0adad8e13e3a157e6a4a42c4f
Geoffrey Donnell <geoffwd@uw.edu>
2021-04-14 18:46:02 -0700

    The 3 functions are complete. Assignment is still in work.

--- series.py
+++ series.py
@@ -24,12 +24,38 @@ def fibonacci(n):
 
 
 def lucas(n):
+    if n == 0:
+        print(2)
+    if n == 1:
+        print(1)
+
+    if n>=2:
+        nth = [0]*(n+1) #the 1st value of the array is index with 0 hence why need the "n+1"
+        nth[0] = 2
+        nth[1] = 1
+        for i in range (2,n+1):
+            nth[i] = nth[i-1] + nth[i-2]
+        print(nth[n])
     pass
 
 
 def sum_series(n, n0=0, n1=1):
+    if n == 0:
+        print(n0)
+    if n == 1:
+        print(n1)
+
+    if n>=2:
+        nth = [0]*(n+1) #the 1st value of the array is index with 0 hence why need the "n+1"
+        nth[0] = n0
+        nth[1] = n1
+        for i in range (2,n+1):
+            nth[i] = nth[i-1] + nth[i-2]
+        print(nth[n])
+
+
     pass
 
 #print(fibonacci.__doc__) #checking to make sure my docstring works
-
-
+fibonacci()
+sum_series(13)
